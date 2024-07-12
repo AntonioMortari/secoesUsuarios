4462a5e1307f6215d7fc714ef77eb6ec3e5e2102
Antonio Marcos Mortari <antonio.mortari@feagri.unicamp.br>
2024-07-12 12:58:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./layouts/Container";
+
+
+const App = () => {
+    return(
+        <Container>
+            <h1>Seção Usuários</h1>
+        </Container>
+    );
+}
+
+export { App };
